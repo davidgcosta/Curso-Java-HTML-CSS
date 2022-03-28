24076a8a39cb31b0f28b8a6a1a089b882f43d791
davidgcosta <davidgeraldocosta@gmail.com>
2022-03-27 21:32:22 -0300

    vscode://vscode.github-authentication/did-authenticate?windowid=1&code=5084a8f89315c069a549&state=b341bebe-fba5-4e93-a334-ac213cab7292

--- Assets/JS/aula29/index.js
+++ Assets/JS/aula29/index.js
@@ -4,29 +4,29 @@ let diaSemanaTexto;
 
 console.log(diaSemana);
  
-switch (diaSemana) {
-case 0:
-     diaSemanaTexto = "Domingo";
-     break;
- case 1:
-     diaSemanaTexto = "Segunda";
-     break;
- case 2:
-     diaSemanaTexto = "Terça";
-     break;
- case 3:
-     diaSemanaTexto = "Quarta";
-     break;
- case 4:
-     diaSemanaTexto = "Quinta";
-     break;
- case 5:
-     diaSemanaTexto = "Sexta";
-     break;
- case 6:
-     diaSemanaTexto = "Sábado";
-     break;
-}
+// switch (diaSemana) {
+// case 0:
+//      diaSemanaTexto = "Domingo";
+//      break;
+//  case 1:
+//      diaSemanaTexto = "Segunda";
+//      break;
+//  case 2:
+//      diaSemanaTexto = "Terça";
+//      break;
+//  case 3:
+//      diaSemanaTexto = "Quarta";
+//      break;
+//  case 4:
+//      diaSemanaTexto = "Quinta";
+//      break;
+//  case 5:
+//      diaSemanaTexto = "Sexta";
+//      break;
+//  case 6:
+//      diaSemanaTexto = "Sábado";
+//      break;
+// }
     
 
 
@@ -48,5 +48,39 @@ case 0:
 //      diaSemanaTexto = 'Insira dados válidos.';
 //  }
 
+function getDiaSemanaTexto (diaSemana) {
+    let diaSemanaTexto;
 
-console.log(diaSemana, diaSemanaTexto)
+    switch (diaSemana) {
+    case 0:
+         diaSemanaTexto = "Domingo";
+         return diaSemanaTexto;
+     case 1:
+         diaSemanaTexto = "Segunda";
+         return diaSemanaTexto;
+     case 2:
+         diaSemanaTexto = "Terça";
+         return diaSemanaTexto;
+     case 3:
+         diaSemanaTexto = "Quarta";
+         return diaSemanaTexto;
+     case 4:
+         diaSemanaTexto = "Quinta";
+         return diaSemanaTexto;
+     case 5:
+         diaSemanaTexto = "Sexta";
+         return diaSemanaTexto;
+     case 6:
+         diaSemanaTexto = "Sábado";
+         return diaSemanaTexto;
+
+    return diaSemanaTexto;
+    }
+}
+
+const diaSemanaTexto1 = getDiaSemanaTexto(diaSemana);
+
+
+
+
+console.log(diaSemana, diaSemanaTexto1);
